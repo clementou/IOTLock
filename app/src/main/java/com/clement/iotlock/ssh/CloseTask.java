917e864ea35433ce111e33708c835a0546ea29bc
clementou <clement.h.ou@gmail.com>
2018-07-20 14:38:09 -0700

    docs

--- app/src/main/java/com/clement/iotlock/ssh/CloseTask.java
+++ app/src/main/java/com/clement/iotlock/ssh/CloseTask.java
@@ -6,7 +6,7 @@ public class CloseTask extends AsyncTask <Void, Void, Void> {
     protected Void doInBackground(Void... command){
         System.out.println("closed");
         String user = "pi";
-        String host = "10.244.0.151";
+        String host = "192.168.43.127";
         String password = "raspberry";
         String knownHosts = "android_asset/known_hosts";
         SSHManager sshManager = new SSHManager(user, password, host, knownHosts);
